9383c00f126f8e5abf6828b0ed38fa747cd9f177
bb93 <zhishiwanwan01@gmail.com>
2025-08-10 15:55:41 +0800

    change the domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://test-fuwri.vercel.app",
+	site: "https://leozblog.vercel.app",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
